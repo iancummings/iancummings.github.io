3399134420bac360e7dc8da7edadfc8022d44327
Ian Cummings <ian.cummings@misc-email.co.uk>
2023-02-26 14:00:22 +0000

    large update before starting to use on anger

--- livetouch/updateFromW.bat
+++
@@ -1,9 +0,0 @@
-REM copy W:\work\doc\asciidoc\livetouch\index.html help
-copy W:\work\doc\asciidoc\livetouch2\contents.html index.html
-copy w:\work\doc\products\projects\LiveTouch2\planning\sprint*.png
-REM cd help
-REM xcopy/Y/S/C/I W:\work\doc\asciidoc\livetouch\*.html
-REM xcopy/Y/S/C/I W:\work\doc\asciidoc\livetouch\*.json
-REM copy/Y W:\work\doc\asciidoc\general\*.html
-REM copy/Y W:\work\doc\asciidoc\general\*.json
-pause
